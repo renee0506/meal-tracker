db8fcef59e71147f7bd3c4f266e36575ef6bc1e6
ReneeMei <meiqianye@gmail.com>
2017-03-31 14:18:00 -0700

    add progress bar to show calories

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,24 +5,33 @@ import { Meal } from './meal.model'
    selector: 'app-root',
    template: `
    <div class="row">
-    <div class="col-md-6">
-    <div *ngIf="!selectedEditMeal">
-      <meal-list [childMealList]='masterMealList' (newSelectionSender)='toggleSelection($event)'></meal-list>
-    </div>
-    <edit-meal [childEditMeal]='selectedEditMeal' (newEditSender)='toggleSelection($event)'></edit-meal>
-    </div>
-    <div class="col-md-6">
-      <div class="jumbotron">
-      <h1>Today's Total:<br> {{getSumCalories()}} calories</h1>
-      </div>
-      <new-meal (newMealSender)='addMeal($event)'></new-meal>
-    </div>
+     <div class="col-md-6" id="left">
+       <div *ngIf="!selectedEditMeal">
+       <meal-list [childMealList]='masterMealList' (newSelectionSender)='toggleSelection($event)'></meal-list>
+       </div>
+       <edit-meal [childEditMeal]='selectedEditMeal' (newEditSender)='toggleSelection($event)'></edit-meal>
+     </div>
+     <div class="col-md-6">
+       <div class="jumbotron">
+        <h2>Today's Consumption</h2>
+         <div class="progress">
+           <div [class]="changeColor()" role="progressbar" [attr.aria-valuenow]="getSumCalories()" aria-valuemin="0" aria-valuemax="2500" style="width: 60%;">
+           {{getSumCalories()}} Calories
+           </div>
+         </div>
+         <div id="labels">
+         <p>0<span id="label">2500</span></p>
+         </div>
+         <h3>{{getAlert()}}</h3>
+       </div>
+       <new-meal (newMealSender)='addMeal($event)'></new-meal>
+     </div>
    </div>
    `
 })
 
 export class AppComponent {
-  masterMealList: Meal[] = [new Meal("Hamburger", "Didn't get a soda or cheese on my burger", 354, "17:24 PM"), new Meal("Fries", "I only ate half of them.", 324, "12:39 PM"), new Meal("Chocolate cake", "Did not eat all the chocolates and cream", 539, "20:30 PM")];
+  masterMealList: Meal[] = [new Meal("Hamburger", "Didn't get a soda or cheese on my burger", 354, "05:24 PM"), new Meal("Fries", "I only ate half of them.", 324, "12:39 PM"), new Meal("Chocolate cake", "Did not eat all the chocolates and cream", 539, "08:30 PM")];
   selectedEditMeal: Meal = null;
 
   addMeal(newMeal: Meal){
@@ -45,6 +54,24 @@ export class AppComponent {
     return sum
   }
 
+  changeColor(){
+    if(this.getSumCalories()<=1500){
+      return "progress-bar progress-bar-success";
+    }else if(this.getSumCalories()<=2000){
+      return "progress-bar progress-bar-warning";
+    }else {
+      return "progress-bar progress-bar-danger";
+    }
+  }
 
+  getAlert(){
+    if(this.getSumCalories()<=1500){
+      return "Keep up with the good work!";
+    }else if(this.getSumCalories()<=2000){
+      return "Be careful with what you eat!";
+    }else {
+      return "Exercise and Veggie!";
+    }
+  }
 
 }
--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,17 +4,21 @@ import {Meal} from './meal.model';
 @Component({
   selector: 'meal-list',
   template: `
-    <ul class="nav nav-tabs" >
-      <li role="presentation" (click)="setFilter('allMeals')" ><a href="#">All Meals</a></li>
-      <li role="presentation" (click)="setFilter('lowerCalories')" ><a href="#">Low Cals</a></li>
-      <li role="presentation" (click)="setFilter('higherCalories')" ><a href="#">High Cals</a></li>
-    </ul>
+    <header>
+      <h2>Today's Food Log</h2>
+      <ul class="nav nav-tabs" >
+        <li role="presentation" (click)="setFilter('allMeals')" ><a href="#">All Meals</a></li>
+        <li role="presentation" (click)="setFilter('lowerCalories')" ><a href="#">Low Cals</a></li>
+        <li role="presentation" (click)="setFilter('higherCalories')" ><a href="#">High Cals</a></li>
+      </ul>
+    </header>
     <ul class="list-unstyled">
       <li *ngFor = 'let meal of childMealList|calories: caloriesFilter'>
-      <h1>{{meal.name}}, <span [class]='changeColor(meal.calories)'>{{meal.calories}}</span> Cals</h1>
+      <h1 class="bg">{{meal.name}}, <span [class]='changeColor(meal.calories)'>{{meal.calories}}</span> Cals</h1>
       <h4>{{meal.time}}</h4>
       <h4>{{meal.details}}</h4>
       <button (click) = 'selectMeal(meal)' class='btn btn-info btn-sm'>Update</button>
+      <hr>
       </li>
     </ul>
   `
