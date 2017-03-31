d251c352aa455dbe86cac4de032d790f8f38dce1
ReneeMei <meiqianye@gmail.com>
2017-03-31 09:10:38 -0700

    add Meal class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model'
 
 @Component({
    selector: 'app-root',
    template: `
-   <h1>Working<h1>
+   <h1>{{testMeal.name}}<h1>
+   <h4>{{testMeal.details}}</h4>
+   <h4>{{testMeal.calories}}</h4>
    `
 })
 
 export class AppComponent {
-  
+  testMeal: Meal = new Meal("Hamburger", "Didn't get a soda or cheese on my burger", 354);
 }
